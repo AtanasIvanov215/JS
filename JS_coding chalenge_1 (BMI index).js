8e991203894a5f61f62083386c0bc49062770318
AtanasIvanov215 <83120646+AtanasIvanov215@users.noreply.github.com>
2021-05-06 17:12:49 +0300

    Add files via upload

---
+++ JS_coding chalenge_1 (BMI index).js
@@ -0,0 +1,8 @@
+let heightMark = 1.88;
+let heightJonh = 1.76;
+let weightMark = 95;
+let weughtJohn = 85;
+let markBMI = weightMark / (heightMark * heightMark);
+let johnBMI = weughtJohn / (heightJonh * heightJonh);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);

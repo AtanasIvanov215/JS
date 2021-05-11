96b6fc6233b1b9ea4b719ab6f7b64266d0f3b11a
AtanasIvanov215 <83120646+AtanasIvanov215@users.noreply.github.com>
2021-05-11 19:40:33 +0300

    Add files via upload

---
+++ JS_coding chalenge_4 (Tip calculator).js
@@ -0,0 +1,15 @@
+let bill = 275;
+let tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill is ${bill}, the tip was ${tip} and the total value ${tip + bill}.`);
+
+//test 2
+
+bill = 40
+tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill is ${bill}, the tip was ${tip} and the total value ${tip + bill}.`);
+
+//test 3
+
+bill = 430;
+tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill is ${bill}, the tip was ${tip} and the total value ${tip + bill}.`);
